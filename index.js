1c0c9441474a93b4704a724c6e6879641981d1c7
mesemi <45022855+mesemi@users.noreply.github.com>
2021-09-02 10:47:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, Intents } = require('discord.js');
+const _ = require('underscore');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+  client.guilds.cache.forEach(guild => {
+		console.log(`${guild.name} | ${guild.id}`);
+    })
+});
+
+client.on('message', message => {
+	if (message.author.bot) return;
+
+	if (message.content == '<@!882990260345249853>') {
+        let theUser = message.guild.members.cache.random().id
+        message.channel.send('<@' + theUser + '>');
+}});
+client.login(process.env.BOTTOKEN)
